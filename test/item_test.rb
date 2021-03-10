f18d40251b97c194e4d40bd7cd331a278610ee1b
Aphilosopher30 <Ashafer1@fordham.edu>
2021-03-10 09:16:04 -0700

    create vendor class

--- test/item_test.rb
+++ test/item_test.rb
@@ -15,7 +15,8 @@ class ItemTest < Minitest::Test
   def test_it_has_attributes
     item1 = Item.new({name: 'Peach', price: "$0.75"})
 
-    assert_instance_of Item, item1
+    assert_equal 'Peach', item1.name
+    assert_equal 0.75 , item1.price
   end
 
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,20 @@ require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+
+
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
+
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    inventory = {}
+
+    assert_equal "Rocky Mountain Fresh" , vendor.name
+    assert_equal inventory, vendor.inventory
+  end
 end
